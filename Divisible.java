945698428ee1664e06d4194ea42c4ea2fabf15e3
Lakshmi9829 <73773463+Lakshmi9829@users.noreply.github.com>
2020-11-03 12:55:46 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,10 @@
+class Divisible{
+	public static void main(String args[]){
+	int numbers[]={10,12,15,22,24,27,33};
+	for(int i=0;i<=7;i++){
+	if(numbers[i]%2==0){
+	System.out.println(numbers[i]);
+	}
+	}
+}
+}
