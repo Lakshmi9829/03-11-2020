9e93b2b18cdb89ec086c7bc62c9a35a1894455de
Lakshmi9829 <73773463+Lakshmi9829@users.noreply.github.com>
2020-11-03 12:58:06 +0530

    Add files via upload

---
+++ NumIsEven.java
@@ -0,0 +1,13 @@
+class NumIsEven{
+	public static void main(String args[]){
+	int number[]={1,14,29,54,43,42,12,17,38,98,60,};
+	for(int i=0;i<11;i++){
+	if(number[i]%2==0){
+	System.out.println( number[i] + "number is even");
+	}
+	else{
+	System.out.println( number[i] + "number is false");
+	}
+	}
+}
+}
