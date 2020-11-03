e36915f74c621fcc908beeb4384704f2dac99d57
Lakshmi9829 <73773463+Lakshmi9829@users.noreply.github.com>
2020-11-03 12:57:18 +0530

    Add files via upload

---
+++ SquareOfNum.java
@@ -0,0 +1,8 @@
+class SquareOfNum{
+	public static void main(String args[]){
+	int array[]={0,1,2,3,4,5,6,7,8,9};
+	for(int i=0;1<10;i++){
+	System.out.println(  array[i] +"\t" + array[i] *array[i]);
+	}
+}	
+}
